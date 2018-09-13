cb1a800021ed52d021d6017c383c27a93a1011b4
avichandan <avichandan.singh@gmail.com>
2018-09-13 11:59:40 +0530

    css changes

--- src/app/performance/performance-graph/performance-graph.component.ts
+++ src/app/performance/performance-graph/performance-graph.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'app-performance-graph',
     templateUrl: 'performance-graph.component.html',
-    styleUrls: ['./performance-graph.component.css']
+    styleUrls: ['./performance-graph.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class PerformanceGraphComponent implements OnInit {
